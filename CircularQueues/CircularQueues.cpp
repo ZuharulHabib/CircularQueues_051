2d0fc0775778a546446bd0d8e24c0197c382c7ba
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2023-06-22 10:36:10 +0700

    mengecek apkah antrian penuh

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -13,5 +13,14 @@ public:
 
 	void insert() {
 		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << endl;
+
+		// Cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overFlow\n";
+			return;
+		}
 	}
 }; 
