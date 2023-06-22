2d3602130ed41e3a5e0b0c1816c5ecbddb8243c7
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2023-06-22 11:24:34 +0700

    membuat switch break

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -114,7 +114,16 @@ int main() {
 			cin >> ch;
 			cout << endl;
 
-
+			switch (ch) {
+			case '1': {
+				
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+			}
+			}
 		}
 	}
 }
