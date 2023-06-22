c099792d210413da4d0cd95010b635df973e428b
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2023-06-22 11:05:46 +0700

    mengecek apakah antrian queue di dalam void diplay kosong atau tidak

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -64,6 +64,12 @@ public:
 		int FRONT_position = FRONT;
 		int REAR_position = REAR;
 
+		//Cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
 
+		}
+		cout << "\nElement is the queue are: \n";
 	}
 }; 
